380724d54f934a860cff02a07b123021df31d46d
Ben <ncor89b@gmail.com>
2024-02-18 20:03:35 +0100

    style(layout.jsx): Adjust code indentation in file

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
   weight: ["400", "500","600", "700"],
-  variable: "--font-ibm-plex" 
+  variable: "--font-ibm-plex",
 });
 
 export const metadata: Metadata = {
