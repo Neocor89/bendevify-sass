95039861ffd2092922ece04aaf558e2c1847692f
Ben <ncor89b@gmail.com>
2024-02-19 21:27:45 +0100

    style([type]/page.tsx): Adjust code indentation in file

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const AddTransformationTypePage = () => {
   return (
@@ -6,4 +6,4 @@ const AddTransformationTypePage = () => {
   )
 }
 
-export default AddTransformationTypePage
+export default AddTransformationTypePage;
