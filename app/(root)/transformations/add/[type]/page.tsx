042c81dc08f507dce09d99b61c261f48c35e7292
Ben <ncor89b@gmail.com>
2024-02-20 21:01:27 +0100

    style([type]/page.tsx): Update TODO comments

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 // TODO
-//: RESTART HERE
+//: STARTING CLERCK SETUP AUTH
+//: ADD CLERCK VARIABLES IN .ENV FILE 
 
 const AddTransformationTypePage = () => {
   return (
