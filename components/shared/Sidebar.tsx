5ce2a75de869a9b07da26f6651a793cb0225c86a
Ben <ncor89b@gmail.com>
2024-02-24 17:51:27 +0100

    build(Sidebar.tsx) Add and positioning logo in component

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Sidebar = () => {
   return (
-    <aside className="sidebar">Sidebar</aside>
+    <aside className="sidebar">
+      <div className="flex size-full flex-col gap-4">
+        <Link href="/" className="sidebar-logo">
+          <Image 
+            src={"/assets/images/logo-text.svg"}
+            alt='Bendevify logo'
+            width={180}
+            height={28}
+          />
+        </Link>
+      </div>
+    </aside>
   )
 }
 
