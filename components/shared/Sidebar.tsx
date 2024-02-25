02931726164c00d9359a628d4d308d6002de2eda
Ben <ncor89b@gmail.com>
2024-02-25 22:31:04 +0100

    build(Sidebar.tsx) NavLinks in component implemented

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,7 +1,14 @@
-import Image from 'next/image';
+"use client";
+
+import { navLinks } from '@/constants';
+import { SignedIn } from '@clerk/nextjs';
+import Image from 'next/image'; 
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <aside className="sidebar">
       <div className="flex size-full flex-col gap-4">
@@ -13,6 +20,24 @@ const Sidebar = () => {
             height={28}
           />
         </Link>
+        
+        <nav className="sidebar-nav">
+          <SignedIn>
+            <ul className="sidebar-nav_elements">
+              {navLinks.map((navLink) => {
+                const isActive = navLink.route === pathname
+
+                return(
+                  <li key={navLink.route} className={`sidebar-nav_element py-1 group ${
+                    isActive ? "bg-purple-gradient text-white" : "text-gray-700"
+                  }`}>
+                    {navLink.label}
+                  </li>
+                )
+              })}
+            </ul>
+          </SignedIn>
+        </nav>
       </div>
     </aside>
   )
