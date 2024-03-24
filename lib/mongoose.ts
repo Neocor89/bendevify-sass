208a9c6752af0ad27946292c19c2ae23a7737640
Ben <ncor89b@gmail.com>
2024-03-24 19:54:11 +0100

    build(mongoose.ts) Finsihed implementing the database connection

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -17,3 +17,25 @@ if (!cached) {
     promise: null
   };
 };
+
+//: VERIFY OR CREATE DATABASE CONNECTION
+export const connectToDatabase = async () => {
+
+  //: CHECK AND RETURN THE CONNECTION
+  if (cached.conn) return cached.conn;
+
+  //: VERIFIED IF THE CACHE OF MONGODB URL EXIST 
+  if (!MONGODB_URL) throw new Error("Your MongoDB URL is missing");
+
+  //: CREATE NEW CACHE PROMISE IF NOT EXIST
+  cached.promise = cached.promise || mongoose.connect(
+    MONGODB_URL, {
+    dbName: "bendevify", bufferCommands: false,
+  });
+
+  //: CREATE THE CONNECTION
+  cached.conn = await cached.promise;
+
+  //: SEND THE CONNECTION TO THE DATABASE
+  return cached.conn;
+};
