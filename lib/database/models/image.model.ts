94925f0e36abd5d57c8ece58c2af0c2c70c8806a
Ben <ncor89b@gmail.com>
2024-03-30 18:23:06 +0100

    build(image.model.ts): First Image schema model created

---
+++ lib/database/models/image.model.ts
@@ -0,0 +1,26 @@
+import { Schema, model, models } from "mongoose";
+
+//* Second Step
+
+//: Creation of ModelSchema
+const ImageSchema = new Schema({
+  title: { type: String, required: true },
+  transformationType: { type: String, required: true },
+  publicId: { type: String, required: true },
+  secureUrl: { type: URL, required: true },
+  width: { type: Number },
+  height: { type: Number },
+  config: { type: Object },
+  transformationUrl: { type: URL },
+  aspectRatio: { type: String },
+  color: { type: String },
+  prompt: { type: String },
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+//: If not use existing model create model named ImageSchema
+const Image = models?.Image || model("Image", ImageSchema);
+
+export default Image;
